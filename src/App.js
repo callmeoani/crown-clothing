9e4e2f0cba070f21768d290dd841003905fb9915
oani <callmeoani@gmail.com>
2021-12-16 20:43:50 +0100

    adding the Header with just the logo, shop and contact link

--- src/App.js
+++ src/App.js
@@ -5,12 +5,13 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop-page-component';
+import Header from './components/header/header-component';
 
 
 function App() {
   return (
     <div>
-      {/* <HomePage /> */}
+      <Header />
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
