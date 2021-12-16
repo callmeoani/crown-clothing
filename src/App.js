376a0f0e645dd592cfe12311ae14942f78354e77
oani <callmeoani@gmail.com>
2021-12-16 16:55:11 +0100

    From the start to the creation of the Shop Page, Collection Preview and Collection Item components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-// import React from 'react';
+
+import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shoppage/shop-page-component';
+
 
 function App() {
   return (
     <div>
-      <HomePage />
+      {/* <HomePage /> */}
+      <Routes>
+        <Route exact path='/' element={<HomePage />} />
+        <Route exact path='/shop' element={<ShopPage />} />
+      </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,19 +1,26 @@
 import React from 'react';
+import { useNavigate } from "react-router-dom";
 import './menu-item.styles2.scss';
 
 
-const MenuItem = ({ title, imageUrl, size }) => (
-    <div className={`${size} menu-item`}
-    >
+const MenuItem = ({ title, imageUrl, size, linkUrl }) => {
+    const navigate = useNavigate();
+
+    return(
+
+        <div className={`${size} menu-item`} onClick={() => navigate(`${linkUrl}`) }
+        >
         <div className='background-image' style={{
-        backgroundImage: `url(${imageUrl})`
+            backgroundImage: `url(${imageUrl})`
         }} 
         />
         <div className='content'>
             <div className='title'>{title.toUpperCase()}</div>
             <span className='subtitle'>SHOP NOW</span>
         </div>
-    </div>
-)
+        </div>
+    )
+
+}
 
 export default MenuItem
