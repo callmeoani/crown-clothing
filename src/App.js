331b7799aba9699cd4121160ca3857148727ae73
oani <callmeoani@gmail.com>
2021-12-18 08:31:50 +0100

    adding form to the app

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shoppage/shop-page-component';
 import Header from './components/header/header-component';
+import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up-component';
 
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route exact path='/' element={<HomePage />} />
         <Route exact path='/shop' element={<ShopPage />} />
+        <Route exact path='/signin' element={<SignInSignUpPage />} />
       </Routes>
     </div>
   );
